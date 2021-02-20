efcee7163882b536f4095b1cc4d8b60ad018f38d
wjlee <instoron@naver.com>
2021-02-20 19:25:35 +0900

    1111

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component
+{
+  render(){
+    return (
+      <div className="gray-background">
+          <img src={logo} lat="logo"/>
+          <h2>Let's develop managment</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
